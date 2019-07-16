4f1546f012636072633e65cb52dd658d5ef9df10
hwasub216 <47962357+hwasub216@users.noreply.github.com>
2019-07-16 14:55:57 +0900

    2.4 Handling Routes with Express finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('HI!');
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res){
+    res.send("Hi form home");
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");
+}
+
+//누군가 URL로 접근
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
+
