2ffa0e8736b2718743bdcf0315829de40659086e
hwasub216 <47962357+hwasub216@users.noreply.github.com>
2019-07-17 20:00:41 +0900

    2.17 Template Variables in Pug finished

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -10,12 +11,14 @@ import routes from "./routes";
 const app = express();
 
 //누군가 URL로 접근
+app.use(helmet()); // 보안
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); // 보안
 app.use(morgan("dev"));// 모든 걸 기록
+app.use(localsMiddleware)
+
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
